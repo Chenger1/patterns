085afcbc95b616de2bea1be4c3edb9cecf21ff51
Chenger1 <exs2199@gmail.com>
2021-03-03 18:05:28 +0200

    Add new version of singleton

--- patterns/singleton.py
+++ patterns/singleton.py
@@ -1,19 +1,32 @@
 class DataBase:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+    # _instance = None
+    #
+    # def __new__(cls, *args, **kwargs):
+    #     if not cls._instance:
+    #         cls._instance = super().__new__(cls)
+    #     return cls._instance
 
     def connect(self):
         print('Connected')
 
 
+class Singleton:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            cls._instance = DataBase()
+        return cls._instance
+
+
 if __name__ == '__main__':
-    db = DataBase()
+    # db = DataBase()
+    # db.connect()
+    # db1 = DataBase()
+    db = Singleton.get_instance()
     db.connect()
-    db1 = DataBase()
+    db1 = Singleton.get_instance()
     print(db is db1)
     print(id(db))
     print(id(db1))
