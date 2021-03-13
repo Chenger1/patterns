7cd7e567b08838788d808e93968ce0ccb13eb5ef
Chenger1 <exs2199@gmail.com>
2021-03-13 19:30:52 +0200

    Add iterator pattern

---
+++ patterns/behaviour/iterator.py
@@ -0,0 +1,50 @@
+from typing import Any, Collection
+
+
+class NewIterator:
+    def __init__(self, iterable: Collection):
+        self._iterable = iterable
+        self._position = 0
+
+    def __next__(self):
+        if self._position < len(self._iterable):
+            elem = self._iterable[self._position]
+            self._position += 1
+            return elem
+        else:
+            raise StopIteration
+
+
+class Stack:
+    def __init__(self):
+        self._stack = []
+
+    def push(self, value: Any):
+        self._stack.append(value)
+
+    def pop(self) -> Any:
+        return self._stack.pop()
+
+    def is_empty(self) -> bool:
+        return len(self._stack) == 0
+
+    def __getitem__(self, item):
+        return self._stack[item]
+
+    # def __iter__(self):
+    #     return NewIterator(self._stack)
+
+    def __str__(self):
+        return f'{self._stack}'
+
+
+if __name__ == '__main__':
+    collection = Stack()
+    for elem in range(1, 10):
+        collection.push(elem)
+    print(collection)
+
+    for elem in collection:
+        for i in collection:
+            print(elem+i)
+
