2608156cf79258e3c4e183902064ed2c3fb611f1
Chenger1 <exs2199@gmail.com>
2021-03-16 20:04:26 +0200

    Delete an extra methods in composite. Change creating elements

--- patterns/structure/composite.py
+++ patterns/structure/composite.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Union, List
 
 
 class MenuComposite(ABC):
@@ -8,10 +7,6 @@ class MenuComposite(ABC):
 
     @abstractmethod
     def show(self, indent: int = 1):
-        pass
-
-    @abstractmethod
-    def return_self_value(self):
         pass
 
     def add(self, child):
@@ -25,52 +20,69 @@ class MenuItem(MenuComposite):
     def show(self, indent: int = 1):
         print(f'{" "*indent} -{self.value}')
 
-    def return_self_value(self):
-        print(f'{self.value}')
-
 
 class MenuList(MenuComposite):
     def __init__(self, value: str):
         super().__init__(value)
         self._children = []
 
-    def add(self, children: Union[MenuComposite, List[MenuComposite]]):
-        if isinstance(children, list):
-            for child in children:
-                self._children.append(child)
-        else:
-            self._children.append(children)
+    def add(self, child: MenuComposite):
+        self._children.append(child)
 
     def remove(self):
         self._children.pop()
 
     def show(self, indent: int = 1):
         print(f'{" " * indent} -*{self.value}')
-        if self._children:
-            for child in self._children:
-                child.show(indent+3)
-        else:
-            print(f'{" " * (indent+3)} -Empty list')
-
-    def return_self_value(self):
-        print(f'{self.value}')
+        for child in self._children:
+            child.show(indent+3)
 
 
 def client(composite: MenuComposite):
     composite.show()
-    print('-------------------\n')
-    composite.return_self_value()
-    print('-------------------\n')
 
 
 if __name__ == '__main__':
-    elements = [MenuItem(f'item{elem}') for elem in range(11)]
-    lists = [MenuList(f'list{elem}') for elem in range(6)]
     main_list = MenuList('Main List')
-    main_list.add(elements[:5])
-    lists[3].add(elements[6])
-    lists[4].add(elements[6:])
-    main_list.add(lists)
+    list1 = MenuList('List-1')
+    list2 = MenuList('List-2')
+    list3 = MenuList('List-3')
+    list4 = MenuList('List - 4')
+
+    elem1 = MenuItem('Elem-1')
+    elem2 = MenuItem('Elem-2')
+    elem3 = MenuItem('Elem-3')
+    elem4 = MenuItem('Elem-4')
+    elem5 = MenuItem('Elem-5')
+    elem6 = MenuItem('Elem-6')
+    elem7 = MenuItem('Elem-7')
+    elem8 = MenuItem('Elem-8')
+    elem9 = MenuItem('Elem-9')
+    elem10 = MenuItem('Elem-10')
+
+# Lists
+    main_list.add(list1)
+    main_list.add(list2)
+
+    list2.add(list3)
+    list2.add(list4)
+
+
+# Elem
+
+    list1.add(elem1)
+
+    list2.add(elem2)
+    list2.add(elem3)
+
+    list3.add(elem4)
+    list3.add(elem5)
+    list3.add(elem6)
+    list3.add(elem7)
+
+    list4.add(elem9)
+    list4.add(elem10)
+
+    main_list.add(elem8)
 
     client(main_list)
-    client(elements[0])
